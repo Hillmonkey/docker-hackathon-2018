dceca974add2a295310220576939c5112751da9f
Larry Madeo <lmadeo76@gmail.com>
2018-04-28 17:15:31 -0700

    message

--- twilio/run-twilio-container.sh
+++ twilio/run-twilio-container.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-docker container run --env TWILIO_ID --env TWILIO_SECRET \
-	--env TWILIO_FROM --env TWILIO_TO larry_sms
+docker container run -p 80:5000 --env TWILIO_ID --env TWILIO_SECRET --env TWILIO_FROM --env TWILIO_TO larry_sms:debstretch
